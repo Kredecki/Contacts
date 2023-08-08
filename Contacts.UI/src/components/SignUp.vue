efc888dd1941739dc35d50c9acb649814debf63e
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-08-08 11:49:32 +0200

    Add register

--- Contacts.UI/src/components/SignUp.vue
+++ Contacts.UI/src/components/SignUp.vue
@@ -1,19 +1,51 @@
 <template>
-
+    <form @submit.prevent="SubmitForm" method="post">
+      <div>
+        <label for="Email">Email</label>
+        <input v-model="email" id="Email" />
+      </div>
+      <div>
+        <label for="Password">Password</label>
+        <input v-model="password" id="Password" type="password" />
+      </div>
+      <div>
+        <label for="Repassword">Password</label>
+        <input v-model="repassword" id="Repassword" type="password" />
+      </div>
+      <input type="submit" value="Register" />
+    </form>
 </template>
 
-<scirpt lang="ts">
-    import { defineComponent } from 'vue';
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import axios from 'axios';
 
-    export default defineComponent({
-        setup(){
-            return{
+  const email = ref('');
+  const password = ref('');
+  const repassword = ref('');
 
-            };
-        };
-    });
-</scirpt>
+  const SubmitForm = async () => {
+    try {
+      await axios.post(
+        "api/SignUp",
+        {
+          Email: email.value,
+          Password: password.value,
+          Repassword: repassword.value
+        },
+        {
+          headers: {
+            Accept: "*/*",
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+</script>
 
-<style>
-@import "../styles/SignUp.scss";
+<style scoped>
+  @import "../styles/SignUp.css";
 </style>
