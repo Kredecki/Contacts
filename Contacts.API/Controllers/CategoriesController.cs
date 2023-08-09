74a32b05ce4d0b3e232ef3e565b39deae0753302
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-08-09 14:23:27 +0200

    Create GetCategories endpoint

--- Contacts.API/Controllers/CategoriesController.cs
+++ Contacts.API/Controllers/CategoriesController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Contacts.API.Interfaces;
+using Contacts.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Contacts.API.Controllers
 {
     public class CategoriesController : Controller
     {
-        
+        private readonly ICategoriesService _categoriesService;
+
+        public CategoriesController(ICategoriesService categoriesService)
+        {
+            _categoriesService = categoriesService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<List<Category>> GetCategories()
+        {
+            return await _categoriesService.GetCategories();
+        }
     }
 }
--- Contacts.API/Services/CategoriesService.cs
+++ Contacts.API/Services/CategoriesService.cs
@@ -1,8 +1,19 @@
 ﻿using Contacts.API.Interfaces;
+using Contacts.API.Models;
 
 namespace Contacts.API.Services
 {
     public class CategoriesService : ICategoriesService
     {
+        private readonly ICategoriesRepositroy _categoriesRepositroy;
+        public CategoriesService(ICategoriesRepositroy categoriesRepositroy)
+        {
+            _categoriesRepositroy = categoriesRepositroy;
+        }
+
+        public async Task<List<Category>> GetCategories()
+        {
+            return await _categoriesRepositroy.GetCategories();
+        }
     }
 }
