37e14c4c45fc82f4287aa41ba5ef4717a4abadc8
Mikołaj Szczepek <m.szczepek01@gmail.com>
2023-08-09 14:23:15 +0200

    Create CategoriesRepository.cs

---
+++ Contacts.API/Data/Repositories/CategoriesRepository.cs
@@ -0,0 +1,21 @@
+﻿using Contacts.API.Interfaces;
+using Contacts.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.API.Data.Repositories
+{
+    public class CategoriesRepository : ICategoriesRepositroy
+    {
+        private readonly DataContext _db;
+
+        public CategoriesRepository(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Category>> GetCategories()
+        {
+            return await _db.Categories.ToListAsync();
+        }
+    }
+}
